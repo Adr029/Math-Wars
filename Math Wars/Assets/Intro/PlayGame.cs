fffc27d2fc2d22c47430d6e0d128729d22851d10
Adrian <adrian.rodriguez.x@bulsu.edu.ph>
2023-02-21 17:38:23 +0800

    animated game intro

--- Math Wars/Assets/Intro/PlayGame.cs
+++ Math Wars/Assets/Intro/PlayGame.cs
@@ -5,9 +5,15 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class PlayGame : MonoBehaviour
 {
-    
+    public GameObject transitionBG;
+    public GameObject creditsPopUp;
     public Image transition;
     public Animator animate;
+
+    void Awake()
+    {
+        transitionBG.SetActive(true);
+    }
     public void Play()
    {
         StartCoroutine(FadeTown());
@@ -17,6 +23,16 @@ public class PlayGame : MonoBehaviour
         animate.SetBool("Fade", true);
         yield return new WaitUntil(() => transition.color.a == 1);
         SceneManager.LoadScene("Math Town");
+    }
+
+    public void ShowCredits()
+    {
+        creditsPopUp.SetActive(true);
+    }
+    public void CloseCredits()
+    {
+        creditsPopUp.SetActive(false);
+
     }
 
 }
