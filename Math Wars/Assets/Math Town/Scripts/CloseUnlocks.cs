10729a49daf35ccd22e712d52fab944519c62642
Adrian <adrian.rodriguez.x@bulsu.edu.ph>
2023-03-06 15:27:35 +0800

    implemented unlocked items prompts

---
+++ Math Wars/Assets/Math Town/Scripts/CloseUnlocks.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseUnlocks : MonoBehaviour
+{
+    public void Close()
+    {
+        Destroy(this.gameObject);
+    }
+}
--- Math Wars/Assets/Math Town/Scripts/PlayerLevel.cs
+++ Math Wars/Assets/Math Town/Scripts/PlayerLevel.cs
@@ -7,12 +7,15 @@ public class PlayerLevel : MonoBehaviour
 {
     public int experience;
     public Text level;
+    public Transform canvas;
+
+    public List<GameObject> NewItems = new List<GameObject>();
     [SerializeField]Slider PlayerXP;
 
     int requiredXP = 100;
 
     public int playerLevel;
-
+    
     void Start()
     {
         
@@ -37,6 +40,25 @@ public class PlayerLevel : MonoBehaviour
             PlayerPrefs.SetInt("Level", playerLevel);
             PlayerPrefs.SetInt("XP", experience);
             level.text =  playerLevel.ToString();
+
+            switch (playerLevel)
+            {
+
+                case 2:
+                    Instantiate(NewItems[0], new Vector3(0,0,0), Quaternion.identity, canvas);
+                break;
+                case 3:
+                    Instantiate(NewItems[1], new Vector3(0,0,0), Quaternion.identity, canvas);
+                break;
+                case 4:
+                    Instantiate(NewItems[2], new Vector3(0,0,0), Quaternion.identity, canvas);
+                break;
+                case 5:
+                    Instantiate(NewItems[3], new Vector3(0,0,0), Quaternion.identity, canvas);
+
+                break;
+  
+            }
         }
 
     }
