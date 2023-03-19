cfc1a98774608654370f8b1393e64e9a8e539e03
sciena114 <abianxyzy0214@gmail.com>
2023-03-19 22:02:06 +0800

    implemented next and back page in library

--- Math Wars/Assets/Formula Library/Scripts/Calculus.cs
+++ Math Wars/Assets/Formula Library/Scripts/Calculus.cs
@@ -6,40 +6,80 @@ public class Calculus : MonoBehaviour
 {
     public List<Sprite> formula = new List<Sprite>();
     public SpriteRenderer calculusFormula;
+    int CalPageCount;
+   [SerializeField]GameObject calNextPageButton;
+   [SerializeField]GameObject calBackPageButton;
 private void Start() 
 {
 
    calculusFormula.sprite = formula[0];
-
+   CalPageCount = 0;    
+   calBackPageButton.SetActive(false);
 }
- public void DifferentiationRules()
- {
-    calculusFormula.sprite = formula[1];
- }
  
- public void DiffTrigo()
- {
-    calculusFormula.sprite = formula[2];
- }
- 
- public void DiffInverseTrigo()
- {
-    calculusFormula.sprite = formula[3];  
- }
+public void NextCalPage()
+{    
+        CalPageCount = CalPageCount + 1;
+     
+        if (CalPageCount == 0)
+        {
+            calculusFormula.sprite = formula[0];            
+            calNextPageButton.SetActive(true);
+            calBackPageButton.SetActive(false);
+        }
+        else if (CalPageCount == 1)
+        {
+            calculusFormula.sprite = formula[1];            
+            calBackPageButton.SetActive(true);
+        }
+        else if (CalPageCount == 2)
+        {
+            calculusFormula.sprite = formula[2];            
+            calBackPageButton.SetActive(true);
+        }
+        else if (CalPageCount == 3)
+        {
+            calculusFormula.sprite = formula[3];
+            calBackPageButton.SetActive(true);
+            calNextPageButton.SetActive(false);
+        }
+}
+ public void BackCalPage()
+{    
+        CalPageCount = CalPageCount - 1;
+     
+        if (CalPageCount == 3)
+        {
+            calculusFormula.sprite = formula[3];
+            calBackPageButton.SetActive(true);
+            calNextPageButton.SetActive(true);
+        }
+        else if (CalPageCount == 2)
+        {
+            calculusFormula.sprite = formula[2];            
+            calBackPageButton.SetActive(true);
+            calNextPageButton.SetActive(true);
+        }
+        else if (CalPageCount == 1)
+        {
+            calculusFormula.sprite = formula[1];            
+            calBackPageButton.SetActive(true);
+            calNextPageButton.SetActive(true);
+        }
+        else if (CalPageCount == 0)
+        {
+            calculusFormula.sprite = formula[0];            
+            calNextPageButton.SetActive(true);
+            calBackPageButton.SetActive(false);
+        }
+}
 
- public void DiffLogarithmic()
- {
-    calculusFormula.sprite = formula[4];
- }
- 
- public void DiffExponential()
- {
-    calculusFormula.sprite = formula[5];
- }
 
  public void ResetCalc()
  {
    calculusFormula.sprite = formula[0];
-
+   CalPageCount = 0;  
+   calBackPageButton.SetActive(false);
+   calNextPageButton.SetActive(true);
  }
 }
--- Math Wars/Assets/Formula Library/Scripts/Trigonometry.cs
+++ Math Wars/Assets/Formula Library/Scripts/Trigonometry.cs
@@ -7,38 +7,79 @@ public class Trigonometry : MonoBehaviour
 
 public List<Sprite> formula = new List<Sprite>();
 public SpriteRenderer trigonometryFormula;
+int TrigoPageCount;
+[SerializeField]GameObject trigoNextPageButton;
+[SerializeField]GameObject trigoBackPageButton;
 
 void Start()
 {
     trigonometryFormula.sprite = formula[0];
+    TrigoPageCount = 0;    
+    trigoBackPageButton.SetActive(false);
 }
-public void Reciprocal()
-{
-    trigonometryFormula.sprite = formula[1];
+
+ public void NextTrigoPage()
+{    
+        TrigoPageCount = TrigoPageCount + 1;
+     
+        if (TrigoPageCount == 0)
+        {
+            trigonometryFormula.sprite = formula[0];            
+            trigoNextPageButton.SetActive(true);
+            trigoBackPageButton.SetActive(false);
+        }
+        else if (TrigoPageCount == 1)
+        {
+            trigonometryFormula.sprite = formula[1];            
+            trigoBackPageButton.SetActive(true);
+        }
+        else if (TrigoPageCount == 2)
+        {
+            trigonometryFormula.sprite = formula[2];            
+            trigoBackPageButton.SetActive(true);
+        }
+        else if (TrigoPageCount == 3)
+        {
+            trigonometryFormula.sprite = formula[3];
+            trigoBackPageButton.SetActive(true);
+            trigoNextPageButton.SetActive(false);
+        }
 }
-public void Quotient()
-{
-    trigonometryFormula.sprite = formula[2];
+ public void BackTrigoPage()
+{    
+        TrigoPageCount = TrigoPageCount - 1;
+     
+        if (TrigoPageCount == 3)
+        {
+            trigonometryFormula.sprite = formula[3];
+            trigoBackPageButton.SetActive(true);
+            trigoNextPageButton.SetActive(true);
+        }
+        else if (TrigoPageCount == 2)
+        {
+            trigonometryFormula.sprite = formula[2];            
+            trigoBackPageButton.SetActive(true);
+            trigoNextPageButton.SetActive(true);
+        }
+        else if (TrigoPageCount == 1)
+        {
+            trigonometryFormula.sprite = formula[1];            
+            trigoBackPageButton.SetActive(true);
+            trigoNextPageButton.SetActive(true);
+        }
+        else if (TrigoPageCount == 0)
+        {
+            trigonometryFormula.sprite = formula[0];            
+            trigoNextPageButton.SetActive(true);
+            trigoBackPageButton.SetActive(false);
+        }
 }
-public void Pythagorean()
-{
-    trigonometryFormula.sprite = formula[3];
-}
-public void SumandDifference()
-{
-    trigonometryFormula.sprite = formula[4];
-}
-public void DoubleAngle()
-{
-    trigonometryFormula.sprite = formula[5];
-}
-public void HalfAngle()
-{
-    trigonometryFormula.sprite = formula[6];
-}
+
  public void ResetTrigo()
  {
-   trigonometryFormula.sprite = formula[0];
-
+    trigonometryFormula.sprite = formula[0];
+    TrigoPageCount = 0;  
+    trigoBackPageButton.SetActive(false);
+    trigoNextPageButton.SetActive(true);
  }
 }
